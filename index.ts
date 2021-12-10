83bd69a6f625088dd3b560207db56730bdd12b23
kalashshah <202051096@iiitvadodara.ac.in>
2021-12-10 10:59:53 +0530

    Add Block

--- index.ts
+++ index.ts
@@ -1,11 +1,29 @@
 import * as crypto from "crypto";
 
 class Transaction {
-  constructor (
+  constructor(
     public amount: number,
     public payer: string,
     public receiver: string
   ) {}
 
   toString = () => JSON.stringify(this);
+}
+
+class Block {
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    // To order blocks in order
+    private timestamp = Date.now()
+  ) {}
+
+  getHash = () => {
+    const str = JSON.stringify(this);
+    // One way secure encryption
+    const hash = crypto.createHash("SHA256");
+    hash.update(str).end();
+    // Hash value as a hexadecimal
+    return hash.digest("hex");
+  };
 }
