a4cfac5600726e9faafcea9336423abc03bcafc9
kalashshah <202051096@iiitvadodara.ac.in>
2021-12-10 11:24:43 +0530

    Add Chain and Wallet

--- index.ts
+++ index.ts
@@ -27,3 +27,48 @@ class Block {
     return hash.digest("hex");
   };
 }
+
+class Chain {
+  // To create a singleton instance
+  public static instance = new Chain();
+  chain: Block[];
+
+  constructor() {
+    // Creating the genesis block
+    this.chain = [new Block("", new Transaction(100, "genesis", "kalash"))];
+  }
+
+  get lastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock = (
+    transaction: Transaction,
+    senderPublicKey: string,
+    signature: string
+  ) => {
+    this.chain.push(new Block(this.lastBlock.prevHash, transaction));
+  };
+}
+
+class Wallet {
+  // To recieve money
+  public publicKey: string;
+  // To spend money
+  public privateKey: string;
+
+  constructor() {
+    // rsa - Full encryption algorithm
+    const keypair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
+    this.privateKey = keypair.privateKey;
+    this.publicKey = keypair.publicKey;
+  }
+
+  sendMoney = (amount: number, payeePublicKey: string) => {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+  };
+}
