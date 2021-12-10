1fe6acef37d0f03cf8863d4c40ace18806fb2052
kalashshah <202051096@iiitvadodara.ac.in>
2021-12-10 23:55:40 +0530

    Add proof-of-work

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ class Transaction {
 }
 
 class Block {
+  public nonce = Math.round(Math.random() * 0xffffffff);
+
   constructor(
     public prevHash: string,
     public transaction: Transaction,
@@ -18,14 +20,14 @@ class Block {
     private timestamp = Date.now()
   ) {}
 
-  getHash = () => {
+  get hash() {
     const str = JSON.stringify(this);
     // One way secure encryption
     const hash = crypto.createHash("SHA256");
     hash.update(str).end();
     // Hash value as a hexadecimal
     return hash.digest("hex");
-  };
+  }
 }
 
 class Chain {
@@ -42,13 +44,40 @@ class Chain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock = (
+  addBlock(
     transaction: Transaction,
     senderPublicKey: string,
-    signature: string
-  ) => {
-    this.chain.push(new Block(this.lastBlock.prevHash, transaction));
-  };
+    signature: Buffer
+  ) {
+    const verify = crypto.createVerify("SHA256");
+    verify.update(transaction.toString());
+    // Verify transaction using signature and public key
+    const isValid = verify.verify(senderPublicKey, signature);
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.mine(newBlock.nonce);
+      this.chain.push(newBlock);
+    } else {
+      console.error("Invalid signature");
+    }
+  }
+
+  // Proof of work
+  mine(nonce: number) {
+    let solution = 1;
+    console.log("Mining...");
+    // Brute force the nonce
+    while (true) {
+      const hash = crypto.createHash("MD5");
+      hash.update((nonce + solution).toString()).end();
+      const attempt = hash.digest("hex");
+      if (attempt.substr(0, 4) === "0000") {
+        console.log("Found a solution!");
+        return solution;
+      }
+      solution++;
+    }
+  }
 }
 
 class Wallet {
@@ -70,5 +99,20 @@ class Wallet {
 
   sendMoney = (amount: number, payeePublicKey: string) => {
     const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+    const sign = crypto.createSign("SHA256");
+    sign.update(transaction.toString()).end();
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
   };
 }
+
+// Usage Example Testing
+const wallet1 = new Wallet();
+const wallet2 = new Wallet();
+const wallet3 = new Wallet();
+
+wallet1.sendMoney(10, wallet2.publicKey);
+wallet2.sendMoney(20, wallet3.publicKey);
+wallet3.sendMoney(30, wallet1.publicKey);
+
+console.log(Chain.instance);
